6f9a5a065f6bc0119d20dfb21a4b49884a631024
EugeniaMatto <eugematto26@gmail.com>
2022-05-11 11:51:25 -0700

    test

--- push.c
+++ push.c
@@ -16,7 +16,7 @@ void _push(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		dprintf(STDERR_FILENO, "L%u: unknown instruction\n", line_number);
+		dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
