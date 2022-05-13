a317768a626505fa0b9d013cb0a6de9c3db32ace
EugeniaMatto <eugematto26@gmail.com>
2022-05-12 17:31:16 -0700

    pchar

--- pchar.c
+++ pchar.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * _pint - prints the value at the top of the stack, followed by a new line.
+ * _pchar - prints the value at the top of the stack, followed by a new line.
  * @stack: stack head
  * @line_number: line num
  * Return: void
@@ -9,16 +9,17 @@
 
 void _pchar(stack_t **stack, unsigned int line_number)
 {
+
 	if (!(*stack))
 	{
 		free(gl.buffer), free_list(*stack), fclose(gl.fd);
 		dprintf(STDERR_FILENO, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n <= 0 || (*stack)->n > 127)
+	if (((*stack)->n) <= 0 || ((*stack)->n) > 127)
 	{
-		dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n", line_number);
-		exit(EXIT_FAILURE);
+	dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n", line_number);
+	exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (*stack)->n);
 }
